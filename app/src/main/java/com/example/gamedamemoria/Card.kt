df3fd8b68fba59a2e6b237d42a7a8401959f1912
Jleandrobr <Joseleandrofmb@gmail.com>
2023-09-21 15:21:21 -0300

    segundo

---
+++ app/src/main/java/com/example/gamedamemoria/Card.kt
@@ -0,0 +1,13 @@
+package com.example.gamedamemoria
+
+class Card(id: Int) {
+    private var id: Int = 0
+    private var capa: Boolean = true
+    private var combinou: Boolean = false
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/gamedamemoria/MainActivity.kt
+++ app/src/main/java/com/example/gamedamemoria/MainActivity.kt
@@ -5,20 +5,60 @@ import android.os.Bundle
 import android.view.View
 import android.widget.GridView
 import android.widget.ProgressBar
+import java.util.Collections
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var gridView: GridView
     private lateinit var progressBar : ProgressBar
+    private val cards = mutableListOf<Card>()
+    private lateinit var primeiroCard: Card
+    private lateinit var segundoCard: Card
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         this.progressBar = findViewById(R.id.progressBar)
+        progress() // progress bar generico
 
-        progress()
+        val cardIds = listOf(
+            R.drawable.gustavo, R.drawable.gustavo,
+            R.drawable.luiz, R.drawable.luiz,
+            R.drawable.leonidas, R.drawable.leonidas,
+            R.drawable.lafayette, R.drawable.lafayette,
+            R.drawable.valeria, R.drawable.valeria,
+            R.drawable.candido, R.drawable.candido,
+            R.drawable.fred, R.drawable.fred,
+            R.drawable.alex, R.drawable.alex,
+        )
+
+        for(id in cardIds) {
+            cards.add(Card(id))
+            cards.shuffle()
+        }
+
+
+
+
+
+//        gridView.setOnItemClickListener{_, view, position, _ ->
+//            val card =
+//        }
+
+
+
+
+
     }
+
+
+
+
+
+
 
 
     /// função da barra de progresso
